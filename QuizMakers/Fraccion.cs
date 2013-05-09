196fc2a3ea53bd548dd02a47ae5a8927dd291a1c
Douglas Guerrero <dguerrero5@hotmail.com>
2013-05-08 17:22:27 -0700

    se creo el proyecto unit testing

--- QuizMakers/Fraccion.cs
+++ QuizMakers/Fraccion.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace QuizMakers
 {
-    class Fraccion
+    public class Fraccion
     {
+        private int _numerador;
+        private int _denominador;
+
+        public Fraccion(int num, int denom)
+        {
+            _numerador=num;
+            _denominador=denom;
+        }
+
+        public Fraccion()
+        {
+            
+        }
+
+        public int getNumerador()
+        {
+            return this._numerador;
+        }
+
+        public int getDenominador()
+        {
+            return this._denominador;
+        }
+
+        public int Sumar(Fraccion f1, Fraccion f2)
+        {
+            return 0;
+        }
     }
 }
--- QuizMakers/Problema.cs
+++ QuizMakers/Problema.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace QuizMakers
 {
-    class Problema
+    public class Problema
     {
+        public Fraccion getOperando1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fraccion getOperando2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public char getOperador()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fraccion getResultado()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
